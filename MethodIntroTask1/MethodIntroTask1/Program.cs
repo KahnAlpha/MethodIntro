a07f67df086a0aba91458633aaaf3645061c8798
KahnAlpha <aykhanmm@code.edu.az>
2023-02-27 17:29:54 -0800

    MethodIntro

---
+++ MethodIntroTask1/MethodIntroTask1/Program.cs
@@ -0,0 +1,44 @@
+﻿void search(int[] array, int number)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == number)
+        {
+            Console.WriteLine(number + " exists in array given");
+            break;
+        }
+        else if (i == array.Length - 1)
+        {
+            Console.WriteLine(number + " no match for element you entered");
+        }
+    }
+}
+
+
+int[] array()
+{
+    Console.Write("enter the number of elements in array: ");
+    int numbersofelements = int.Parse(Console.ReadLine());
+
+    int[] arr = new int[numbersofelements];
+
+    Console.WriteLine("enter the elements of array");
+    for (int i = 0; i < numbersofelements; i++)
+    {
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+
+    return arr;
+}
+
+
+int enternumber()
+{
+    Console.Write("Enter the number you're searching for search: ");
+    return int.Parse(Console.ReadLine());
+}
+
+
+int[] arr = array();
+int N = enternumber();
+search(arr, N);
---
+++ MethodIntroTask2/MethodIntroTask2/Program.cs
@@ -0,0 +1,11 @@
+﻿int biggest(params int[] arr)
+{   int max = arr[0]; 
+    for(int i=1;i<arr.Length;i++)
+    {
+        if (arr[i]>max)
+        { max = arr[i]; }   
+    }
+    return max;
+}
+Console.WriteLine("biggest number among given is:") ;
+Console.WriteLine(biggest(50,80,56,45,81,25,-55,45,87)); 
